bd215d4bf8e5048b144a2752955e81be92d546dc
jiayu_guo <jiayu_guo@paradise.com.tw>
2020-05-18 09:35:58 +0800

    fix webhook

--- bot/updateMessage.go
+++ bot/updateMessage.go
@@ -13,7 +13,7 @@ func UpdateMessage() {
 	u := tgbotapi.NewUpdate(0)
 	u.Timeout = 60
 
-	updates := BotFB.ListenForWebhook("/hook")
+	updates := BotFB.ListenForWebhook("/")
 	go http.ListenAndServe("https://fb-crawler-oaov.herokuapp.com/hook", nil)
 	/*updates, err := BotFB.GetUpdatesChan(u)
 	if err != nil {
